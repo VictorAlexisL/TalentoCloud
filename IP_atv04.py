33508b12d4a282f676a73a000983af63bc3c6e5a
Victor <139696656+VictorAlexisL@users.noreply.github.com>
2023-11-10 15:56:13 -0300

    Create IP_atv04.py

---
+++ IP_atv04.py
@@ -0,0 +1,21 @@
+#Repetição com for
+for i in range(1,21):
+  if (i != 13):
+    print(i)
+
+
+#Repetição com while
+i = 0
+
+while i < 20:
+  i += 1
+  if (i != 13):
+    print(i)
+
+#Desafio com ordem reversa
+i = 20
+
+while i > 0:
+  if (i != 13):
+    print(i)
+  i -= 1
