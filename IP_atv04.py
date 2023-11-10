99f40dff94f098c98b0c94dd816d4072a00179c8
Victor <139696656+VictorAlexisL@users.noreply.github.com>
2023-11-10 16:05:41 -0300

    Update IP_atv04.py

--- IP_atv04.py
+++ IP_atv04.py
@@ -3,7 +3,6 @@ for i in range(1,21):
   if (i != 13):
     print(i)
 
-
 #Repetição com while
 i = 0
 
@@ -12,7 +11,12 @@ while i < 20:
   if (i != 13):
     print(i)
 
-#Desafio com ordem reversa
+#Desafio com ordem reversa - for
+for i in range(20,0,-1):
+  if (i != 13):
+    print(i)
+    
+#Desafio com ordem reversa - while
 i = 20
 
 while i > 0:
