3e6257742a68ef11cb746dc532667b31f1658c60
Victor <139696656+VictorAlexisL@users.noreply.github.com>
2023-11-10 04:41:35 -0300

    Create IP_atv02.py

---
+++ IP_atv02.py
@@ -0,0 +1,11 @@
+def calculadora(x, y, operador):
+  if (operador == '+'):
+    return x + y
+  elif (operador == '-'):
+    return x - y
+  elif (operador == '/'):
+    return x / y
+  elif (operador == '*') or (operador == 'x'):
+    return x * y
+  else:
+    return "Operador inválido. Apenas soma, subtração, multiplicação e divisão são suportadas."
